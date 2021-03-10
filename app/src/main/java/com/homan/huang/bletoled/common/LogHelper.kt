82382e2f16e86a7815229f5e758be2c2a0eabaff
Homan Huang <homanhuang@gmail.com>
2021-03-10 09:39:25 -0800

    1.21 Add lgv lgw

--- app/src/main/java/com/homan/huang/bletoled/common/LogHelper.kt
+++ app/src/main/java/com/homan/huang/bletoled/common/LogHelper.kt
@@ -6,3 +6,5 @@ const val TAG = "MLOG"
 fun lgd(s:String) = Log.d(TAG, s)
 fun lgi(s:String) = Log.i(TAG, s)
 fun lge(s:String) = Log.e(TAG, s)
+fun lgv(s:String) = Log.v(TAG, s)
+fun lgw(s:String) = Log.w(TAG, s)
